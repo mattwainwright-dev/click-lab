0ffdfa3fcab732d527df155f3f859cd6230cb557
Matthew Wainwright <mwwainwright1014@gmail.com>
2026-08-23 00:59:36 -0500

    finish and polish pulse quiz

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let headline = document.getElementById("headline-1");
-console.log(headline);
 let answer1 = document.getElementById("answer-1");
 let answer2 = document.getElementById("answer-2");
 let answer3 = document.getElementById("answer-3");
+let answer4 = document.getElementById("answer-4");
+let answer5 = document.getElementById("answer-5");
+let answer6 = document.getElementById("answer-6");
+let answer7 = document.getElementById("answer-7");
+let answer8 = document.getElementById("answer-8");
+let answer9 = document.getElementById("answer-9");
 let hintButton = document.getElementById("hint-button");
 let hintText = document.getElementById("hint-text");
 let score = 0;
 let scoreText = document.getElementById("score-text");
 let answeredCorrectly = false;
+let answeredCorrectly2 = false;
+let answeredCorrectly3 = false;
 
 let feedback = document.getElementById("feedback-1");
+let feedback2 = document.getElementById("feedback-2");
+let feedback3 = document.getElementById("feedback-3");
 
 answer1.addEventListener("click", function () {
     feedback.textContent = "Correct!";
@@ -27,6 +36,40 @@ answer2.addEventListener("click", function() {
 
 answer3.addEventListener("click", function() {
     feedback.textContent = "Not this one!";
+});
+
+answer4.addEventListener("click", function() {
+    feedback2.textContent = "Not this one!";
+});
+
+answer5.addEventListener("click", function() {
+    feedback2.textContent = "Correct!";
+    if (answeredCorrectly2 === false) {
+        score = score + 1;
+        scoreText.textContent = "Score: " + score;
+        answeredCorrectly2 = true;
+    }
+});
+
+answer6.addEventListener("click", function() {
+    feedback2.textContent = "Nope — try again!";
+});
+
+answer7.addEventListener("click", function() {
+    feedback3.textContent = "Not this one!";
+});
+
+answer8.addEventListener("click", function() {
+    feedback3.textContent = "Correct!";
+    if (answeredCorrectly3 === false) {
+        score = score + 1;
+        scoreText.textContent = "Score: " + score;
+        answeredCorrectly3 = true;
+    }
+});
+
+answer9.addEventListener("click", function() {
+    feedback3.textContent = "Nope — try again!";
 });
 
 hintButton.addEventListener("click", function () {
